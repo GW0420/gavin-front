8ecbef2bc98677631e65e55380f03d74e40a41f7
“wei <116019284@qq.com>
2022-06-26 10:06:22 +0800

    feat: 项目初始化

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path, { join } from 'path'
 
-// https://vitejs.dev/config/
+const alias = {
+  '@': 'src'
+}
+
 export default defineConfig({
+  // 软连接
+  resolve: {
+    alias: {
+      '@': join(__dirname, '/src')
+    }
+  },
+  // 端口代理地址
+  server: {
+    port: '8989',
+    host: '0.0.0.0'
+  },
   plugins: [vue()]
 })
