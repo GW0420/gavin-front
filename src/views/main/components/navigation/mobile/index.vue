8a90a98ce5d5bb846736f565e0b775331aaca0b2
“wei <116019284@qq.com>
2022-06-29 22:54:41 +0800

    feat: 移动端 slider 处理

--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="bg-white sticky top-10 left-0 z-10">
-    <ul class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden">
+    <ul ref="ulTarget" class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden">
+      <!-- 滑块 -->
+      <li ref="sliderTarget" class="absolute h-[22px] bg-zinc-900 rounded-lg duration-200" :style="sliderStyle"></li>
       <!-- 汉堡按钮 -->
       <li class="z-20 fixed top-0 right-[-1px] h-4 px-1 flex items-center bg-white shadow-l-white">
         <gw-icon class="w-1.5 h-1.5" name="hamburger"></gw-icon>
       </li>
-      <li class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4" v-for="item in data" :key="item.id">
+      <!-- category item -->
+      <li
+        class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4"
+        :class="[currentCategoryIndex === index ? 'text-zinc-100' : '']"
+        :ref="setItemRef"
+        v-for="(item, index) in data"
+        :key="item.id"
+        @click="onItemClick(index)"
+      >
         {{ item.name }}
       </li>
     </ul>
@@ -13,10 +23,52 @@
 </template>
 
 <script setup>
+import { ref, onBeforeUpdate, watch } from 'vue'
+import { useScroll } from '@vueuse/core'
+
 defineProps({
   data: {
     type: Array,
     required: true
   }
 })
+
+// 滑块
+const sliderStyle = ref({
+  transform: 'translateX(0px)',
+  width: '60px'
+})
+// 选中的 item 下标
+const currentCategoryIndex = ref(0)
+// 获取填充的所有 item 元素
+let itemRefs = []
+const setItemRef = el => {
+  if (el) {
+    itemRefs.push(el)
+  }
+}
+// 数据改变之后， DOM变化之前
+onBeforeUpdate(() => {
+  itemRefs = []
+})
+// 获取 ul 元素，以计算偏移位置
+const ulTarget = ref(null)
+
+const { x: ulScrollLeft } = useScroll(ulTarget)
+watch(currentCategoryIndex, val => {
+  // 获取ul内容去padding值
+  const { paddingLeft } = window.getComputedStyle(ulTarget.value, null)
+  // 获取选中元素的 left、width
+  const { left, width } = itemRefs[val].getBoundingClientRect()
+  // 为 sliderStyle 设置属性
+  sliderStyle.value = {
+    // ul 横向滚动位置 + 当前元素的 left 偏移量 + ul内容区 padding
+    transform: `translateX(${ulScrollLeft.value + left - parseInt(paddingLeft) + 'px'})`,
+    width: width + 'px'
+  }
+})
+// item 点击事件
+const onItemClick = index => {
+  currentCategoryIndex.value = index
+}
 </script>
