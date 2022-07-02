8733c27164d9097c82c857c066e8e04b6b36f66d
wei_gu <116019284@qq.com>
2022-07-02 09:04:38 +0800

    PC 端 Header 模块

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>layout {{ isMobileTerminal }}</div>
+  <div class="h-screen">
+    <Header class="h-header"></Header>
+    <Main class="h-main"></Main>
+    <Floating class="fixed bottom-10 right-2"></Floating>
+  </div>
 </template>
 
 <script setup>
-import { isMobileTerminal } from '@/utils/flexible'
+import Header from './components/header/index.vue'
+import Main from './components/main.vue'
+import Floating from './components/floating.vue'
 </script>
 
 <style lang="scss" scoped></style>
